e4bf309cb047c4b99dbdcf2d12f88067ea83e572
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-02-16 00:12:17 -0600

    Update and rename scratch_1.py to EfficientPrimeFactorization.py

--- EfficientPrimeFactorization.py
+++ EfficientPrimeFactorization.py
@@ -1,15 +1,15 @@
 #Efficient Prime Factorization
 
 n=int(input("Enter the number: "))
-if n==1 :  #because the below logic doesn't work on 1
+if n==1 : 
     print([n])
 for i in range(2 , n+1):
     if n%i==0 :
-        n1=i  #get factor
-        for b in range(2,n+1): #check if it is prime
+        n1=i  
+        for b in range(2,n+1): 
             if ((n1%b)==0) & (n1==b):
                 print([n1])
-            elif (n1%b)==0 or n1<b:  #if not then pass
+            elif (n1%b)==0 or n1<b:  
                 break
 
 
