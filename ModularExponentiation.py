9813f768fe03c1d0d81b182e0bd304be5cf69626
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-02-21 22:35:02 -0600

    Update ModularExponentiation.py

--- ModularExponentiation.py
+++ ModularExponentiation.py
@@ -1,12 +1,11 @@
 #Modular Exponentiation in Python 
 # then applies % operator
 
-
 a = 2
 b = 100
-p = (int)(1e9+7)
+c = (int)(1e9+7)
 
 # Using direct fast method to compute 
-# (a ^ b) % p.
-d = pow(a, b, p)
+# (a ^ b) % c.
+d = pow(a, b, c)
 print (d)
