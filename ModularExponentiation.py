ae323bc7b7c1651929cac70f1144dcdb4ea7776d
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-02-22 22:54:11 -0600

    Update ModularExponentiation.py

--- ModularExponentiation.py
+++ ModularExponentiation.py
@@ -1,21 +1,12 @@
 # Modular Exponentiation in Python 
 
-""" 
-n = 8765
-power = 101
-mod = 9691573
-
-x = 0
-while x < mod:
-    if (x ** power) % mod == n:
-        break
-    x += 1
-
-print (x)
-
-"""
 x = 7
 y = 2
 z = 5
 
+# pow() doing =  x^y % z 
+
 print(pow(x, y, z))
+
+
+
