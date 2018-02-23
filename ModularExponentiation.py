4cfaced42fb9fa46c3e5834c4594f244cc1eb4b1
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-02-22 23:19:50 -0600

    Update ModularExponentiation.py

--- ModularExponentiation.py
+++ ModularExponentiation.py
@@ -1,12 +1,13 @@
 # Modular Exponentiation in Python 
+# pow(x, y, z,)  = x ** y % z
 
-x = 7
-y = 2
-z = 5
 
-# pow() doing =  x^y % z 
+def modexp(x, y, z):
+  x = (x**y)%z
+  print(x)
+  #print(pow(x, y, z))
 
-print(pow(x, y, z))
+modexp(5, 4, 7)
 
 
 
