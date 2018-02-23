c1d31cb0c202521af2f93737e0037f13d3a18051
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-02-23 00:17:43 -0600

    Update PowerOfLargeNumbers.py

--- PowerOfLargeNumbers.py
+++ PowerOfLargeNumbers.py
@@ -1,6 +1,9 @@
-def pwlargenum():
-    n = int(input())
-    power = int(input())
-    n = n ** power
-    print(n)
-pwlargenum()
+# Power Of Large Numbers HackerRank
+
+def pwrlargenum():
+    t=(int)(input())
+    for i in range(0,t):
+        a,b=(input().split())
+        print(pow((int)(a),(int)(b),1000000007))
+        
+pwrlargenum()
