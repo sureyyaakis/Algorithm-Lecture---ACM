aa33d49c95231845481389780295e3e12e562a48
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-02-22 21:40:26 -0600

    Update LeastCommonMultiple(LCM).py

--- LeastCommonMultiple(LCM).py
+++ LeastCommonMultiple(LCM).py
@@ -1,20 +1,15 @@
 #Least Common Multiple (LCM)
 
-def lcm(x, y):
-  
+def lcm(x, y):  
   if x > y:
     z = x
-  
   else:
     z = y
-  
-  while(True):
-  
+  while(True):  
     if(( z%x == 0 )and (z%y == 0)):
       lcm = z
       break
-    z+=1
-  
+    z+=1  
   return lcm
   
 print(lcm(3,7))
