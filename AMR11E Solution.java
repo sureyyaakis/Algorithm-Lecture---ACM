357d3ba794fd230446bf67c61d1245d0824fd0a2
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-03-18 12:35:33 -0500

    Update and rename AMR11E Solution.py to AMR11E Solution.java

--- AMR11E Solution.java
+++ AMR11E Solution.java
@@ -1,3 +1,4 @@
+/**
 """
 Algorithm:
 1- Initialise an integer array (till 2700) to 0.
@@ -13,4 +14,39 @@ for num in range(2, 2701):
 
 print(primes[3:1001])
 print("The index: ",len(primes[3:1001])) # There arent 1000 prime numbers between 0 to 2700
+
+*/
+
+// not done!
+
+// make a prime list
+// n = input [1,1001]
+// T = test case how many n [1,21]
+// find at least three distinct prime factors[ex: 1, 30(2,3,5)]
+
+ 
+class AMR11E {
+  public static void main(String[] args) {
+ 
+  List<Integer> primes = new ArrayList<Integer>();
+  
+  
+  for(int i=2; i<=100; i++){
+    
+      for(int j=2; j<=i; j++){
+        if(j==i) 
+        {
+          System.out.println(i);
+          primes.add(i);
+        }
+        if(i%j == 0)
+        break;
+      }
+    
+  }        
+ 
+  System.out.println(primes);
+  
+  }
+}
 
