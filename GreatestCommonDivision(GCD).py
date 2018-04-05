fd04de5c9f880f670924c32026dd39e7f3f3c1a3
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-04-05 00:05:47 -0500

    Update GreatestCommonDivision(GCD).py

--- GreatestCommonDivision(GCD).py
+++ GreatestCommonDivision(GCD).py
@@ -1,16 +1,13 @@
 # Greatest Common Division (GCD)
+# With subtract 
 
-def gcd(x, y):
-    gcd = 1
-    
-    if x % y == 0:
-        return y
-    
-    for k in range(int(y / 2), 0, -1): 
-        if x % k == 0 and y % k == 0: # common divisor
-            gcd = k
-            break  
-    return gcd
-
-print(gcd(12, 17))
-print(gcd(4, 6))
+def gcd(a,b):
+  
+  while(a != b):
+    if (a>b):
+      a = a-b
+    else:
+      b = b-a
+  return a
+  
+print(gcd(252, 105))
